8fff781cbb7677e49fa42b9e6bcd5233cd229c0f
Anu Oyebanjo <anuoyebanjodunmi660@gmail.com>
2023-06-17 04:16:23 +0100

    first commit

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Test.css"
+
+function Test() {
+  return (
+    <h1 className="test">I am a Test</h1>
+  )
+}
+
+export default Test
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCchQD5azfFmMD_3joQZfMK1V1y0S_tv7c",
+    authDomain: "netflix-clone-a4fa2.firebaseapp.com",
+    projectId: "netflix-clone-a4fa2",
+    storageBucket: "netflix-clone-a4fa2.appspot.com",
+    messagingSenderId: "23690108745",
+    appId: "1:23690108745:web:f509636ef8739d36200498"
+  };
